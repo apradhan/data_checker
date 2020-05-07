dffa3f8592c60b2d122f222e936d9bc168bd0912
Anup Pradhan <anupprad@gmail.com>
2020-05-07 18:16:26 +0000

    data checker stuff

--- data_checker/items.py
+++ data_checker/items.py
@@ -1,14 +1,6 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
-class DataCheckerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Dataset(scrapy.Item):
+    name = scrapy.Field()
+    link = scrapy.Field()
+    organization = scrapy.Field()
